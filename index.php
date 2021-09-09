e1ee9fdb1027bfeaee5c435b8b9fac266037f082
Malezhyk Mihail <malezhik@intertool.kharkov.ua>
2021-09-09 17:41:40 +0300

    pre complite application

--- index.php
+++ index.php
@@ -1,27 +1,2 @@
 <?php
 require_once "core/startup.php";
-
-//use core\libs\DotEnv;
-//use core\libs\Config;
-
-//use core\engine\Loader;
-//use core\services\Export;
-
-//print_r(get_class_methods(Export::class));
-
-//require_once "vendor/autoload.php";
-//require_once "core/startup.php";
-//require_once "core/libs/DotEnv.php";
-//
-//use core\libs\DotEnv;
-//$dotEnv = new DotEnv(APP_DIR);
-//
-//echo "<pre>";
-//try {
-//    $dotEnv->parseEnv();
-//    var_dump($_ENV);
-//} catch (Exception $e) {
-//    echo $e->getMessage();
-//}
-
-//\routes\Route::getInstance()->load();
